a148f686e0fa21b4d5782b47ae1cbb63bcef8fed
priyankaKanwal <priyankaka@interrait.com>
2026-09-03 17:34:57 +0530

    all

--- app/main.py
+++ app/main.py
@@ -11,12 +11,15 @@ app = FastAPI(
     description="Backend API for Inventory and Stock Management",
     version="1.0.0"
 )
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:4200"
+        "http://localhost:4200",
+        "http://127.0.0.1:4200",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
